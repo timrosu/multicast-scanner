76fc751b2c40108d30050c0d602488f2ec5c286d
Tim <49987073+timrosu@users.noreply.github.com>
2026-03-29 21:45:11 +0200

    fixed up script
    - optimised logic
    - made output less verbose
    - improved readability
    
    TODOs:
    - eliminate all newline Nones in output
    - fix adding external playlist

--- multicast-scanner.py
+++ multicast-scanner.py
@@ -1,3 +1,4 @@
+import logging
 import signal
 import concurrent.futures
 import argparse
@@ -17,9 +18,9 @@ parser.add_argument("--range", help="Range of IPs to scan.", required=True)
 parser.add_argument("--size", help="Size of the subnets to divide.", required=False, default='32')
 parser.add_argument("--playlist", help="Playlist *.m3u file with UDP streams", required=False)
 parser.add_argument("--nic", help="network interface IP address with UDP stream", required=False, default='0.0.0.0')
-parser.add_argument("--udp_timeout", help="Time to wait in seconds for the UPD port reply", required=False, default=5)
+parser.add_argument("--udp_timeout", help="Time to wait in seconds for the UPD port reply", required=False, default=1)
 parser.add_argument("--port", help="addtional UDP port to scan. Default: 1234", required=False, default=['1234'], nargs='+')
-parser.add_argument("--info_timeout", help="Time to wait in seconds for the stream's info", required=False, default=10)
+parser.add_argument("--info_timeout", help="Time to wait in seconds for the stream's info", required=False, default=6)
 parser.add_argument("--threads", help="Number of threads to use", required=False, default=10)
 
 # Define the variable for the channels dictionary
@@ -35,28 +36,28 @@ def create_file(ip_range):
     file_name = f'scan_results_range_{network.network_address}-{network.prefixlen}.m3u'
     file = os.path.join(current_path, file_name)
     with open(file, 'w') as f:
-        f.write(f'#EXTM3U\n')
+        f.write('#EXTM3U\n')
     return file_name, file
 
-def playlist_add(ip, port, name, file, channels_dict):
+def playlist_add(ip, port, name, file, channels_dict): # TODO: fix adding external playlist
     """ Add the given IP and port to the playlist file"""
-    if type(name) is not str:
-        channel_string = f'#EXTINF:2,Channel: {ip}:{port}\n'
-    else:
+    if isinstance(name,str):
         channel_string = f'#EXTINF:2,{name}\n'
+    else:
+        channel_string = f'#EXTINF:2,Channel: {ip}:{port}\n'
+
     with open(file, 'a') as f:
         if args.playlist:
             if f'{str(ip)}:{port}' not in list(channels_dict.values()):
                 f.write(channel_string)
-                f.write(f'rtp://{ip}:{port}\n')
+                f.write(f'udp://{ip}:{port}\n')
                 print(f'[*] !!! Channel added to the playlist. {ip}:{port} >>> {name} !!!')
                 return 0
-            else:
-                print(f'[*] The channel is already in the playlist: {ip}:{port} >>> {name}')
-                return 0
+            print(f'[*] The channel is already in the playlist: {ip}:{port} >>> {name}')
+            return 0
         f.write(channel_string)
-        f.write(f'rtp://{ip}:{port}\n')
-    print(f'[*] !!! Channel added to the playlist. {ip}:{port} >>> {name} !!!')
+        f.write(f'udp://{ip}:{port}\n')
+    # print(f'[*] !!! Channel added to the playlist. {ip}:{port} >>> {name} !!!')
     return 0
 
 def socket_creator(nick, port):
@@ -72,30 +73,49 @@ def socket_creator(nick, port):
 def channel_checker(sock, arguments):
     """Check the channel for the stream info"""
     try:
-        ready = select.select([sock], [], [], arguments.info_timeout)
+        timeout=int(arguments.info_timeout)
+        ready = select.select([sock],[],[],timeout)
         if ready[0]:
             sock.close()
             return True
-        else:
-            sock.close()
-            return False
+        sock.close()
+        return False
     except (socket.error, select.error) as exception:
         print(f"Error: {exception}")
         return False
 
-def get_ffprobe(address, port, arguments):
+def get_ffprobe(address,port,nic,info_timeout):
     """ To get the json data from ip:port """
+
+    cmd = [
+        'ffprobe', 
+        '-v', 'quiet', 
+        '-print_format', 'json', 
+        '-show_programs', 
+        '-localaddr', nic,
+        f'udp://{address}:{port}'
+    ]
+
     try:
         result = subprocess.run(
-            ['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_programs', f'udp://{address}:{port}'],
-            capture_output=True, text=True, timeout=arguments.info_timeout)
-        json_string = json.loads(str(result.stdout))
-        channel_name = json_string['programs'][0]['tags']['service_name']
+            cmd,
+            capture_output=True,
+            text=True,
+            timeout=info_timeout,
+            check=True
+        )
+
+        # json_string = json.loads(str(result.stdout))
+        channel_name = json.loads(result.stdout)['programs'][0]['tags']['service_name']
         print(f'[*] Channel: {channel_name} found on {address}:{port}')
         return channel_name
-    except (subprocess.CalledProcessError, json.JSONDecodeError) as exception:
-        print(f'[*] Error: {exception}')
-        return 0
+    except subprocess.TimeoutExpired:
+        print(f"[*] No channel found on {address}:{port} in {info_timeout} seconds")
+    except subprocess.CalledProcessError as e:
+        print(f"FFprobe failed with exit code {e.returncode}: {e.stderr}")
+    except json.JSONDecodeError:
+        print("Failed to parse FFprobe output as JSON")
+    return None # TODO: eliminate all newline Nones in output
 
 def ip_scanner(ip_addresses, ports):
     """Scan the IP address for the UDP streams"""
@@ -104,16 +124,15 @@ def ip_scanner(ip_addresses, ports):
             sock = socket_creator(args.nic, port)
             result = channel_checker(sock, args)
             if result:
-                print(f'[*] Channel found on {ip}:{port}')
-                channel_name = get_ffprobe(ip, port, args)
+                # print(f'[*] Channel found on {ip}:{port}')
+                channel_name = get_ffprobe(ip, port, args.nic, args.info_timeout)
                 if channel_name:
                     playlist_add(ip, port, channel_name, playlist_file, channels_dictionary)
-                else:
-                    print(f'[*] Channel found on {ip}:{port} but no name found.')
-                    print(f'[*] You can add the channel manually to the playlist.')
+                # else:
+                    # print(f'[*] Channel found on {ip}:{port} but no name found.')
+                    # print(f'[*] You can add the channel manually to the playlist.')
             else:
                 print(f'[*] No channel found on {ip}:{port}')
-                pass
 
 
 def handler(signalname):
@@ -134,13 +153,13 @@ if args.port:
 try:
     ip_list = ipaddress.IPv4Network(args.range)
 except ValueError:
-    print(f'\n[*] Please define a proper IP range.\n[*] >>> Example: 224.0.0.0/24\n')
-    exit()
+    print('\n[*] Please define a proper IP range.\n[*] >>> Example: 224.0.0.0/24\n')
+    sys.exit()
 
 if ip_list.is_multicast:
     pass
 else:
-    print(f'[*] IPs provided are not multicast. Please try again.')
+    print('[*] IPs provided are not multicast. Please try again.')
     sys.exit()
 
 try:
@@ -153,7 +172,7 @@ except ValueError:
 try:
     arg_max_workers = int(args.threads)
 except ValueError:
-    print(f'[*] ERROR: the number of threads must be an integer.\n')
+    print('[*] ERROR: the number of threads must be an integer.\n')
     sys.exit()
 
 playlist_file_name, playlist_file = create_file(args.range)
@@ -164,7 +183,7 @@ try:
         for item in concurrent.futures.as_completed(results):
             print(item.result())
 except (KeyboardInterrupt, SystemExit):
-    print(f'[*] Exiting...')
+    print('[*] Exiting...')
     raise
 except Exception as e:
     print(f'[*] ERROR: {e}')
